b9c02117b50e5243e6c74bccd43bdf27769a0719
lapphong <lapphong012@gmail.com>
2023-02-07 19:52:32 +0700

    add subscribe

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import MainCarousel from '../../pages/home/MainCarousel';
 import ShoppingList from './ShoppingList';
+import Subscribe from './Subscribe';
 
 export default function Home() {
    return (
       <div className="home">
          <MainCarousel />
          <ShoppingList />
+         <Subscribe />
       </div>
    );
 }
