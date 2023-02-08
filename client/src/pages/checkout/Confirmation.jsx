fd5385c19b409f95c0731fd399b8a5013eb7d69f
lapphong <lapphong012@gmail.com>
2023-02-08 11:50:33 +0700

    add check out and payment

--- client/src/pages/checkout/Confirmation.jsx
+++ client/src/pages/checkout/Confirmation.jsx
@@ -1,3 +1,15 @@
+import { Box } from '@mui/material';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+
 export default function Confirmation() {
-   return <div>Confirmation</div>;
+   return (
+      <Box m="90px auto" width="80%" height="50vh">
+         <Alert severity="success">
+            <AlertTitle>Success</AlertTitle>
+            You have successfully made an Order —{' '}
+            <strong>Congrats on Making your Purchase</strong>
+         </Alert>
+      </Box>
+   );
 }
